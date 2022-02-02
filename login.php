7ac3c4c966b6d49c645871f33217a10f63214f02
SamuelKocifaj <75687509+SamuelKocifaj@users.noreply.github.com>
2022-02-02 11:34:58 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>login</title>
+</head>
+<body>
+
+
+
+<?php
+if($_POST["uname"] == "kockifaj12@gmail.com" && $_POST["psw"]== ("samko1062" ))
+{
+echo "Prihlasenie je uspesne!";
+}
+else {
+    echo"Nespravne udaje";
+}
+?>
+</body>
+</html>
